97be5fd98f8db5f201f83b59bbc86381c0225c6e
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2024-10-05 00:16:11 +0530

    change the tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,35 @@
-import type { Config } from "tailwindcss";
+// import type { Config } from "tailwindcss";
 
-const config: Config = {
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         customColor1: "rgba(19, 17, 50, 1)",
+//         customColor2: "rgba(96, 90, 197, 1)",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        customColor1: "rgba(19, 17, 50, 1)",
-        customColor2: "rgba(96, 90, 197, 1)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-export default config;
+}
