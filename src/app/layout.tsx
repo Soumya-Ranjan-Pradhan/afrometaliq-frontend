0930f3212f0c2369b4aa41f79f8a4e6f6fea1568
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2024-10-06 23:40:11 +0530

    added the buttton tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <HomeBanner />
         <div className="sm:flex flex-none">
           <div>
-            <Sidebar />
+            {/* <Sidebar /> */}
           </div>
           <div className="sm:flex-1">
             {children}
