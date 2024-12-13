4f560f40455a293aee28f8d4f40eb727c9f282f1
Sourav Ojha <subhamojha9@gmail.com>
2024-12-13 16:53:05 +0530

    display category on homepage

--- src/Components/Category/index.tsx
+++ src/Components/Category/index.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useCategories } from "@/api/category/queries/useCategoryQuery";
 import Image from "next/image";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { FaLongArrowAltLeft } from "react-icons/fa";
@@ -8,82 +10,84 @@ interface CategoryItem {
   imageUrl: string;
 }
 
-const categories: CategoryItem[] = [
-  {
-    id: 1,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 2,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 3,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 4,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 5,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 6,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 7,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 8,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 9,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 10,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 11,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-  {
-    id: 12,
-    title: "Steel",
-    imageUrl:
-      "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
-  },
-];
+// const categories: CategoryItem[] = [
+//   {
+//     id: 1,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 2,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 3,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 4,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 5,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 6,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 7,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 8,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 9,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 10,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 11,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+//   {
+//     id: 12,
+//     title: "Steel",
+//     imageUrl:
+//       "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png",
+//   },
+// ];
 
 const Category: React.FC = () => {
+  const { data } = useCategories({ parent: null });
+
   return (
     <div className="py-8">
       <div className="flex justify-center my-10 items-center gap-2 mt-4">
@@ -106,23 +110,26 @@ const Category: React.FC = () => {
 
       <div className="overflow-x-auto flex space-x-6 px-6 hide-scrollbar">
         {/* Category items */}
-        {categories.map((category) => (
-          <div key={category.id} className="flex-shrink-0 text-center">
-            <div className="w-24 h-24 md:w-28 md:h-28 lg:w-44 lg:h-44 rounded-full border-4 border-[#5A43AF] flex items-center justify-center mx-auto  transition-colors duration-300">
-              <Image
-                src={category.imageUrl}
-                alt={category.title}
-                width={100}
-                height={100}
-                className="rounded-full object-cover"
-              />
-            </div>
+        {data?.data.categories &&
+          data.data.categories.map((category) => (
+            <div key={category._id} className="flex-shrink-0 text-center">
+              <div className="w-24 h-24 md:w-28 md:h-28 lg:w-44 lg:h-44 rounded-full border-4 border-[#5A43AF] flex items-center justify-center mx-auto  transition-colors duration-300">
+                <Image
+                  src={
+                    "https://res.cloudinary.com/dndq25au1/image/upload/v1729446209/lsuk3ft68aqyq5se4wvc.png"
+                  }
+                  alt={category.category_name}
+                  width={100}
+                  height={100}
+                  className="rounded-full object-cover"
+                />
+              </div>
 
-            <p className="mt-2 text-2xl md:text-md font-medium">
-              {category.title}
-            </p>
-          </div>
-        ))}
+              <p className="mt-2 text-2xl md:text-md font-medium">
+                {category.category_name}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );
