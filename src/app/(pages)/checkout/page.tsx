336480fc748179ad66a401be23d6c3c6f62e90d0
Sourav Ojha <subhamojha9@gmail.com>
2024-12-27 23:10:34 +0530

    chekout stripe page

---
+++ src/app/(pages)/checkout/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { useSearchParams } from "next/navigation";
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+);
+
+console.log("Stripe key", process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+export default function Checkout() {
+  //get client secret fron location state
+  const clientSecret = useSearchParams().get("scs"); // scs - stripe client secret
+
+  if (!clientSecret)
+    return (
+      <div>
+        <p>Missing client secret</p>
+      </div>
+    );
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret,
+  };
+
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <CheckoutForm />
+    </Elements>
+  );
+}
