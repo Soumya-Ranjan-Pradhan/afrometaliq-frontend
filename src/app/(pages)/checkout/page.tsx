6a117436a259a4e59a720865a11667cb7d208dd9
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2025-01-19 19:52:19 +0530

    quatition

--- src/app/(pages)/checkout/page.tsx
+++ src/app/(pages)/checkout/page.tsx
@@ -1,37 +1,50 @@
-"use client";
+// "use client";
 
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-import CheckoutForm from "./CheckoutForm";
-import { useSearchParams } from "next/navigation";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+// import CheckoutForm from "./CheckoutForm";
+// import { useSearchParams } from "next/navigation";
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
-);
+// // Make sure to call `loadStripe` outside of a component’s render to avoid
+// // recreating the `Stripe` object on every render.
+// const stripePromise = loadStripe(
+//   process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
+// );
 
-console.log("Stripe key", process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+// console.log("Stripe key", process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
-export default function Checkout() {
-  //get client secret fron location state
-  const clientSecret = useSearchParams().get("scs"); // scs - stripe client secret
+// export default function Checkout() {
+//   //get client secret fron location state
+//   const clientSecret = useSearchParams().get("scs"); // scs - stripe client secret
 
-  if (!clientSecret)
-    return (
-      <div>
-        <p>Missing client secret</p>
-      </div>
-    );
+//   if (!clientSecret)
+//     return (
+//       <div>
+//         <p>Missing client secret</p>
+//       </div>
+//     );
 
-  const options = {
-    // passing the client secret obtained from the server
-    clientSecret,
-  };
+//   const options = {
+//     // passing the client secret obtained from the server
+//     clientSecret,
+//   };
 
+//   return (
+//     <Elements stripe={stripePromise} options={options}>
+//       <CheckoutForm />
+//     </Elements>
+//   );
+// }
+
+
+import React from 'react'
+
+const page = () => {
   return (
-    <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
-    </Elements>
-  );
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  )
 }
+
+export default page
