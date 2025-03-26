c13d589b0983a513d83a32e4b9dad22aac59e5a9
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2025-03-27 00:05:18 +0530

    fixed issue

--- src/Components/ITCompany/DiscussSection/index.tsx
+++ src/Components/ITCompany/DiscussSection/index.tsx
@@ -4,10 +4,11 @@ import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
+
 const DiscussSection = () => {
   return (
-    <section className="relative items-center justify-center  md:flex-row md:justify-between">
-               {/* Top Line with Icon */}
+    <section className="relative items-center justify-center md:flex-row md:justify-between">
+      {/* Top Line with Icon */}
       <div className="absolute top-0 flex items-center justify-center w-full">
         <div className="h-[2px] w-3/4 bg-gray-300 relative">
           <div className="absolute -top-2 left-1/2 transform -translate-x-1/2 bg-red-500 w-6 h-6 rounded-full flex items-center justify-center">
@@ -20,14 +21,15 @@ const DiscussSection = () => {
           </div>
         </div>
       </div>
+
       <div className="text-center flex items-center justify-center py-7">
         <h2 className="text-4xl font-bold text-gray-900">
-          Let's <span className="text-red-500">Discuss</span>
+          Let&apos;s <span className="text-red-500">Discuss</span>
         </h2>
       </div>
+
       <div className="relative flex flex-col md:flex-row items-center justify-center md:justify-between p-8 bg-white">
         {/* Left Side Content */}
-
         <div className="relative flex justify-center w-full md:w-1/2">
           <motion.div
             className="relative p-4 border-2 border-dashed border-red-400 rounded-full"
@@ -52,12 +54,12 @@ const DiscussSection = () => {
           </p>
           <p className="text-gray-700 mt-2">
             With our experienced team of IT professionals, we have the expertise
-            & knowledge to tackle projects of varying sizes.
+            &amp; knowledge to tackle projects of varying sizes.
           </p>
           <Link href="/contact">
-          <button className="mt-4 bg-gray-900 text-white px-6 py-2 rounded-lg hover:bg-gray-700 transition">
-            Contact us
-          </button>
+            <button className="mt-4 bg-gray-900 text-white px-6 py-2 rounded-lg hover:bg-gray-700 transition">
+              Contact us
+            </button>
           </Link>
         </div>
       </div>
