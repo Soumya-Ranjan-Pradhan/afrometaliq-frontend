68b5de9247fdfee5e473bd46509030a13ae55dd8
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2025-06-14 01:18:44 +0530

    dsfd

--- src/api/auth/authApi.ts
+++ src/api/auth/authApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001/api/v1";
+import { BASE_URL } from "@/contants";
 // import { BASE_URL } from "@/contants";
 import { clearLS, getFromLS } from "@/lib/storage";
 export interface User {
