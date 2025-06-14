ab09a3434edb8c2f376410e1f6d13577e823f323
Soumya-Ranjan-Pradhan <soumyapradhan63711@gmail.com>
2025-06-14 16:04:20 +0530

    change env

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
   return (
     <>
       <Header />
+      {/* {comingSoon && <ComingSoonModal onClose={handleClose} />} */}
 
       <div>
         <Sidebar />
